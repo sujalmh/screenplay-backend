4b401f25da5c5c4ad646b91b12b270eaa0a1e42b
sujalmh9@gmail.com <sujalmh>
2024-10-21 05:34:33 +0530

    CORS

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from pytz import timezone
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 app.config['JWT_SECRET_KEY'] = 'Num3R0n4u7s!Num3R0n4u7s!'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=6)
